49ff7ddd95734a611d051140716e8409c840b476
ludahong35 <ludahong35@163.com>
2018-07-26 21:05:54 +0800

    add project

---
+++ glmain.cpp
@@ -0,0 +1,17 @@
+
+#include "glmain.h"
+#include "moc_glmain.h"
+#include "moc_predefs.h"
+
+GlMainWindow::GlMainWindow(QWidget *parent) : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+GlMainWindow::~GlMainWindow()
+{
+}
+void GlMainWindow::Init()
+{
+    
+}
